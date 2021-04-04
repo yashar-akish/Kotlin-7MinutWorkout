3b25a8714dbcf918537e50fb29e7665de6ef975c
yashar-akish <yashar_akish@yahoo.com>
2021-04-04 14:42:23 -0400

    adding second page using intent and adding toolbar and toolbar theme to it

---
+++ app/src/main/java/com/example/a7minuteworkout/ExerciseActivity.kt
@@ -0,0 +1,21 @@
+package com.example.a7minuteworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_exercise.*
+
+class ExerciseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exercise)
+
+        setSupportActionBar(toolbar_exercise_activity)
+        val actionbar = supportActionBar
+        if (actionbar != null){
+            actionbar.setDisplayHomeAsUpEnabled(true)
+        }
+        toolbar_exercise_activity.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+}
--- app/src/main/java/com/example/a7minuteworkout/MainActivity.kt
+++ app/src/main/java/com/example/a7minuteworkout/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.a7minuteworkout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -11,7 +12,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         ll_start.setOnClickListener {
-            Toast.makeText(this, "Start Now", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, ExerciseActivity::class.java)
+            startActivity(intent)
         }
     }
 }
