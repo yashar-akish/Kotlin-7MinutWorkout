29b3905d04a7d123ac834692a4eb7da58b221efe
yashar-akish <yashar_akish@yahoo.com>
2021-04-04 12:00:43 -0400

    main page UI

--- app/src/main/java/com/example/a7minuteworkout/MainActivity.kt
+++ app/src/main/java/com/example/a7minuteworkout/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.a7minuteworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ll_start.setOnClickListener {
+            Toast.makeText(this, "Start Now", Toast.LENGTH_SHORT).show()
+        }
     }
 }
